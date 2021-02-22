7f5f7e3f448e4f072cf45fa5276733d2a7d824ce
Rui Coelho <ruicoelho@ua.pt>
2021-02-22 20:33:31 +0000

    Structs for Books and Authors

--- main.go
+++ main.go
@@ -6,11 +6,31 @@ import (
 	"net/http"
 )
 
+type Book struct {
+	ID string `json:"id"`
+	ISBN string `json:"isbn"`
+	Title string `json:"title"`
+	Author *Author `json:"author"`
+}
+
+type Author struct {
+	FirstName string `json:"first_name"`
+	LastName string `json:"last_name"`
+}
+
 func getBooks(w http.ResponseWriter, r *http.Request)  {}
+func getBook(w http.ResponseWriter, r *http.Request)  {}
+func createBook(w http.ResponseWriter, r *http.Request)  {}
+func updateBook(w http.ResponseWriter, r *http.Request)  {}
+func deleteBook(w http.ResponseWriter, r *http.Request)  {}
 
 func handleRequests()  {
 	myRouter := mux.NewRouter()
 	myRouter.HandleFunc("/books", getBooks).Methods("GET")
+	myRouter.HandleFunc("/books/{id}", getBook).Methods("GET")
+	myRouter.HandleFunc("/books", createBook).Methods("POST")
+	myRouter.HandleFunc("/books/{id}", updateBook).Methods("PUT")
+	myRouter.HandleFunc("/books/{id}", deleteBook).Methods("DELETE")
 	log.Fatal(http.ListenAndServe(":8000", myRouter))
 }
 func main(){
